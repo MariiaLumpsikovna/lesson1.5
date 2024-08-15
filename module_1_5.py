2d1297d2d8420fb883aa93e81f7af5537303ea37
MariiaLumpsikovna <mboorakova@gmail.com>
2024-08-15 22:34:26 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = 'star', 5, 3.6, False
+print(immutable_var)
+immutable_var[1] = 'wood'
+print(immutable_var)
+
+
+mutable_list = [45, 'finger', 9.1, True]
+mutable_list[3] = 6
+mutable_list[0] = 'string'
+print(mutable_list)
